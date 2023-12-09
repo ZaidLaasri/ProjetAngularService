8ab44fbff99ca322896fdf20d6cc3732c9afc36a
Your Name <zayd.laasri@gmail.com>
2023-12-09 17:14:46 +0100

    first commit

---
+++ model/assignment.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let AssignmentSchema = Schema({
+    id: Number,
+    dateRendu: Date,
+    nom: String,
+    rendu: Boolean,
+    note: Number,
+});
+
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+module.exports = mongoose.model('Assignment', AssignmentSchema);
